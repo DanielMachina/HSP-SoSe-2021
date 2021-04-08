dfc86fc487f14336b8bdc184d93a3e9f209c509b
DanielMachina <80002944+DanielMachina@users.noreply.github.com>
2021-04-08 15:34:34 +0200

    Die ersten Schritte
    Guck in readme, und lies die Kommentare :)

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # HSP-SoSe-2021
+
+
+Hallo Leute,
+
+schreibt einfach hier mal kurz rein was ihr geändert habt und wie man evtl. weiter machen könnte. (Mit Datum)
+Bitte den Code immer mit Kommentaren versehen! :)
+
+Daniel 08.04 
+Ich habe eine Klasse "Schraubendefinition" erstellt.
+Diese hat bisher die Eigenschaft Material, welches nur übergeben wird. 
+Es gibt noch die Eigenschaft getDichte, da wird jedem Material eine jeweilige Dichte hinzugefügt.
+Wenn jemand weiter machen will, kann er z.B mehr Materialien hinzufügen (Messing usw.), oder 
+sonst kann man auch schon weitere Eigenschaften mitgeben (Länge, Gewinde,Nenndurchmesser usw.)
--- Schrauben/Schrauben/Program.cs
+++ Schrauben/Schrauben/Program.cs
@@ -9,10 +9,57 @@ namespace Schrauben
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("HelloWorld");
+        {          
+            Schraubendefinition eins = new Schraubendefinition();
+            //Material wird definiert
+            eins.Material = "Stahl";
+
+            //Asugabe
+            Console.WriteLine(eins.getDichte(eins.Material));
             Console.ReadKey();
 
+
         }
+       
+
+
     }
+
+    public class Schraubendefinition //hier wird die Schraube definiert
+    {  
+        public string Material //Eigenschaft Material. Wird in main definiert
+        { get; set; }
+
+        public double getDichte(string material) // Methode um Dichte des jeweiligen Materials zu bekommen
+        {
+            double spezDichte = 0;
+            switch (Material)
+            {
+                case "Stahl": //Jeder case kann ein Material annehmen
+                    Console.WriteLine("Das Material ist " + Material);
+                    spezDichte = 7.85; // Hier bekommt  das Material seine Dichte
+                    break;
+                case "Aluminium":
+                    Console.WriteLine("Das Material ist " + Material);
+                    spezDichte = 2.7;
+                    break;
+                case "Titan":
+                    Console.WriteLine("Das Material ist " + Material);
+                    spezDichte = 4.507;
+                    break;
+                    //Hier können mehr Materialien rein
+
+                default: // Falls ein nicht aufgelistetes Material benutzt wird wird default ausgegeben
+                    Console.WriteLine("Material leider nicht vorhanden, prüfe deine Rechtschreibung: " + Material);
+                    break;
+
+            }
+
+            Console.Write("Die Dichte ist " );
+            return spezDichte;
+
+        }
+
+    }
+
 }
